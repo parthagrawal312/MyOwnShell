d232dbed87b1ce05ee327e685158e6d1d09a85ec
Parth Agrawal <parthagrawal@Parths-MacBook-Air.local>
2025-01-16 16:07:23 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,41 @@ int main() {
     cout << "$ ";
     string input;
     getline(cin, input);
-    if(input== "exit 0")
-    {
-      return 0;
+
+
+    vector<string> split_sentence(string sen) {
+      vector<string> userinput;
+      string word = "";
+      for (char c : input) {  // Iterate through each character in the user input sentence
+        if (c == ' ') {  
+            userinput.emplace_back(word);// If a space is found, add the word to the vector
+            word = "";// Reset the word
+        }
+        else {
+            word += c;// Append the character to the current word
+        }
+      }
+      if (!word.empty()) { // Add the last word to the vector
+        userinput.emplace_back(word);
+      } 
+      return userinput;// Return the vector containing words
     }
-    cout<<input<<": command not found"<<endl;
+
+    vector<string> userinput = split_sentence(input);
+    switch(userinput[0]){
+      case "exit":
+        return 0;
+      case "echo":
+        for(int i=1; i<userinput.size(); i++)
+        {
+          cout<<userinput[i]<<" ";
+        }
+        break;
+      default:
+        cout<<input<<": command not found"<<endl;
+        break;
+    }
+    
   }
   
 }
