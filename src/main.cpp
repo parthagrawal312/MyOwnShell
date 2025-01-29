e8309bcb56a78c55eed9b8648716b2fa1b109416
Parth Agrawal <parthagrawal@Parths-MacBook-Air.local>
2025-01-29 15:28:53 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -142,13 +142,8 @@ int main() {
         bool sig=0;
         for(int i=1; i<userinput.size(); i++)
         {
-          if(userinput[i]==" ")
-          {
-            continue;
-          }
-          else{
-            cout<<userinput[i]<<" ";
-          }
+          
+            cout<<"[]"<<userinput[i]<<" ";
           
           // for(char c : userinput[i])
           // {
