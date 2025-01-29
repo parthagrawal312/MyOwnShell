795d9da993ea69e32a7ae0f43a549945a7b68fbb
Parth Agrawal <parthagrawal@Parths-MacBook-Air.local>
2025-01-29 15:44:04 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -59,6 +59,7 @@ vector<string> split_sentence(string input) {
         // }
           // Iterate through each character in the user input sentence
         if (openquote==false && c == ' ') {
+            cout<<"emplacedWord is :"<<word<<endl;
             userinput.emplace_back(word);// If a space is found, add the word to the vector
             word = "";// Reset the word
         }
